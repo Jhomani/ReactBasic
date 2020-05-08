862d2e572c2febe5330d55315afc672913ee86b0
JuankaMF <juancarlos69528125@gmail.com>
2020-05-07 21:57:21 -0400

    trabajo finalizado

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,35 +1,60 @@
 import React from 'react'
-import './styles/BadgesList.css'
 import { Link } from 'react-router-dom'
-import Gravatar from './Gravatar'
+import BadgeItem from './BadgeItem'
 
-class BadgesList extends React.Component{
-    render(){
-        if(this.props.badges.length === 0){
-            return (
-                <div className="Badges__container">
-                    <h3>No badges were found</h3>
-                    <Link to="badges/new" className="btn btn-primary">Creade new badge</Link>
-                </div>
-            )
-        }
-        return(
+function useSearchBadges(badges){
+    const[query, setQuery] = React.useState('') 
+    const [filteredBadges, setFilteredBadges] = React.useState(badges);
 
-            <div className="Badges__list">
+    React.useMemo(()=>{
+        const result = badges.filter(badge=>`${badge.firstName}${badge.lastName}`.toLowerCase().includes(query.toLowerCase()))
+        setFilteredBadges(result)
+    }, [badges, query]);
+
+    return { query, setQuery, filteredBadges }
+}
+
+function BadgesList (props){
+    const badges = props.badges
+    const{query, setQuery, filteredBadges} = useSearchBadges(badges) 
+
+    if(filteredBadges.length === 0){
+        return (
+                    <div className="container">
+                        <div className="form-group">
+                            <label>Filter Badges</label>
+                            <input type="text" className='form-control'
+                                value={query}
+                                onChange={e => {
+                                    setQuery(e.target.value)
+                                }}
+                            />
+                        </div>
+
+                        <h3>No badges were found</h3>
+                        <Link to="badges/new" className="btn btn-primary">Creade new badge</Link>
+                    </div>
+                )
+    }
+    return(
+        <div className="container">
+            <div className="form-group">
+                <label>Filter Badges</label>
+                <input type="text" className='form-control'
+                    value={query}
+                    onChange={e => {
+                        setQuery(e.target.value)
+                    }}
+                />
+            </div>
+
             <div className="Badges__container">
                 <ul>
-                    {this.props.badges.map(item => {    
+                    {filteredBadges.map(item => {    
                         return(
                             <li className="item" key={item.id} >
                                 <Link className="text-reset text-decoration-none Badges__item" to={`badges/${item.id}`}>
-                                    <div className="Badges__img">
-                                        <Gravatar email={item.email} alt="avatar" />
-                                    </div>
-                                    <div className="Badges__data">
-                                        <p className="Badges__data-name">{item.firstName} {item.lastName}</p>
-                                        <span >@{item.twitter}</span>
-                                        <p>{item.jobTitle}</p>
-                                    </div>
+                                    <BadgeItem badge = {item}/>
                                 </Link>
                             </li>
                         )
@@ -37,8 +62,7 @@ class BadgesList extends React.Component{
                 </ul>
             </div>
         </div>
-        )
-    }
+    )
 }
 
 export default BadgesList; 
--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -3,10 +3,22 @@ import { Link } from 'react-router-dom'
 import Badge from '../components/Badge'
 import logoConf from '../images/platziconf-logo.svg'
 import './style/BadgeDetails.css'
-import ReactDom from 'react-dom'
+import DeleteBadgeModal from '../components/DeleteBadgeModal';
 
+
+// function useIncreaseCount(max){
+//     const [state, setState] = React.useState(0);
+
+//     if(state > max){
+//         setState(0)
+//     }
+
+//     return [state, setState]
+// }
 
 const BadgeDetails = (props) => {
+    // const [state, setState] = useIncreaseCount(4);
+    
     const data = props.badge;
     
     const {firstName, lastName, email, twitter, jobTitle, id} = data
@@ -40,10 +52,17 @@ const BadgeDetails = (props) => {
                         <div className="col">
                             <h2>Actions</h2>
 
-                            <div><Link className="btn btn-primary mb-4" to={`/badges/${id}/edit`}>Edit</Link></div>
                             <div>
-                                <button className="btn btn-danger ">Delete</button>
-                                {/* {ReactDom.createPortal(que, donde)} */}
+                                {/* <button onClick={()=>{setState(state+1)}} className="btn btn-primary mr-4">Increase Count: {state}</button> */}
+                                <Link className="btn btn-primary mb-4" to={`/badges/${id}/edit`}>Edit</Link>
+                            </div>
+                            <div>
+                                <button onClick={props.onOpenModal} className="btn btn-danger ">Delete</button>
+                                <DeleteBadgeModal 
+                                isOpen={props.modalIsOpen} 
+                                onClose = {props.onCloseModal}
+                                onDeleteBadge = {props.onDeleteBadge}
+                                />,
                             </div>
 
                         </div>
